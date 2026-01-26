d5959ac3b4d580bbf8a6c3d4fc3cf81ace485e8f
박종국 <whdrnrdl789@naver.com>
2026-01-26 19:50:43 +0900

    first commit

---
+++ src/main/java/com/subject/codesmellsolver/CodeSmellSolverApplication.java
@@ -0,0 +1,13 @@
+package com.subject.codesmellsolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSmellSolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSmellSolverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/subject/codesmellsolver/CodeSmellSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subject.codesmellsolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSmellSolverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
